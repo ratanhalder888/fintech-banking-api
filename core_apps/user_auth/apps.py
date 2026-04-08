a5cbf9c6193c93fe880db17427c35541edab16cd
ratan <ratanhalder60@gmail.com>
2026-04-08 12:11:43 +0600

    chore: add default_auto_field to Django app configs

--- core_apps/user_auth/apps.py
+++ core_apps/user_auth/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserAuthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = 'core_apps.user_auth'
     verbose_name = _("User Auth")
