4ac5e50bc459f384eb174d9e277303bea8d2a779
ratan <ratanhalder60@gmail.com>
2026-04-29 16:08:29 +0600

    chore(settings): align staticfiles directory

--- config/settings/base.py
+++ config/settings/base.py
@@ -158,8 +158,8 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = str(BASE_DIR / "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
