00ae8c5158c52726e0d45d5f7b48d3f6a96734a5
ratan <ratanhalder60@gmail.com>
2026-05-07 10:20:08 +0600

    feat: configure cloudinary

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,6 +16,7 @@ from os import getenv, path
 from loguru import logger
 import sys
 from datetime import timedelta
+import cloudinary
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -196,6 +197,18 @@ CELERY_TASK_TIME_LIMIT = 5 * 60
 CELERY_TASK_SOFT_TIME_LIMIT = 60
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_SEND_SENT_EVENT = True
+
+
+CLOUDINARY_CLOUD_NAME = getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECERET = getenv("CLOUDINARY_API_SECERET")
+
+
+cloudinary.config(
+    cloud_name = CLOUDINARY_CLOUD_NAME,
+    api_key = CLOUDINARY_API_KEY,
+    api_secret = "CLOUDINARY_API_SECERET",
+ )
 
 
 LOGGING_CONFIG = None
