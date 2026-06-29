498e645740924f378db56b8b303597037336e06b
ratan <ratanhalder60@gmail.com>
2026-06-29 16:43:52 +0600

    fix(settings): add 127.0.0.1:8080 to CSRF_TRUSTED_ORIGINS

--- config/settings/local.py
+++ config/settings/local.py
@@ -30,7 +30,7 @@ DOMAIN = getenv("DOMAIN")
 
 MAX_UPLOAD_SIZE = 1 * 1024 * 1024
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080"]
 
 LOCKOUT_DURATION = timedelta(minutes=1)
 
