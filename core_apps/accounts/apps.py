1ae2862105c1d6b83ea62c4bd8a1fed6081af68e
ratan <ratanhalder60@gmail.com>
2026-07-08 11:17:27 +0600

    fix(accounts): correct AppConfig name to match installed app path
    name='accounts' mismatched INSTALLED_APPS entry 'core_apps.accounts',
    causing ImproperlyConfigured at startup.

--- core_apps/accounts/apps.py
+++ core_apps/accounts/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 class AccountsConfig(AppConfig):
-    name = 'accounts'
+    name = 'core_apps.accounts'
     default_auto_field = "django.db.models.BigAutoField"
     verbose_name = _("Accounts")
