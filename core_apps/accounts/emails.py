c0657a4dc82e17c38de4302f81eb96de92744cab
ratan <ratanhalder60@gmail.com>
2026-07-15 15:34:13 +0600

    feat(accounts): add KYC verification serializer and account activation email

--- core_apps/accounts/emails.py
+++ core_apps/accounts/emails.py
@@ -28,3 +28,21 @@ def send_account_creation_email(user, bank_account):
         logger.error(
             f"Failed to send account created email to {email}: Error: {str(e)}"
         )
+
+
+def send_full_activation_email(account: BankAccount) -> None:
+    subject = _("Your Bank Account is now fully activated")
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [account.user.email]
+    context = {"account": account, "site_name": settings.SITE_NAME}
+    html_email = render_to_string("emails/bank_account_activated.html", context)
+    plain_email = strip_tags(html_email)
+    email = EmailMultiAlternatives(subject, plain_email, from_email, recipient_list)
+    email.attach_alternative(html_email, "text/html")
+    try:
+        email.send()
+        logger.info(f"Account Fully Activated email sent to: {account.user.email}")
+    except Exception as e:
+        logger.error(
+            f"Failed to send Fully Activated email to  {account.user.email}: Error: {str(e)}"
+        )
