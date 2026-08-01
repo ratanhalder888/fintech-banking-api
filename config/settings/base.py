dc07f9c795da80c24ef94823ccafe4495aa38319
ratanhalder888 <ratanhalder888@gmail.com>
2026-08-01 23:48:00 +0600

    fix(settings): use corrected Cloudinary secret in client config

--- config/settings/base.py
+++ config/settings/base.py
@@ -288,7 +288,7 @@ CLOUDINARY_API_SECRET = getenv("CLOUDINARY_API_SECRET")
 cloudinary.config(
     cloud_name = CLOUDINARY_CLOUD_NAME,
     api_key = CLOUDINARY_API_KEY,
-    api_secret = CLOUDINARY_API_SECERET,
+    api_secret = CLOUDINARY_API_SECRET,
 )
 
 
